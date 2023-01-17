76d349140a12bcb57fbe32b8a2086436f339a72d
AlexandrLytkin <Dracca@mail.ru>
2023-01-17 11:11:15 +0300

    курсовая комит 2

--- src/main/java/com/example/coursework3/exception/InSufficientSockQuantityException.java
+++
@@ -1,7 +0,0 @@
-package com.example.coursework3.exception;
-
-public class InSufficientSockQuantityException extends RuntimeException{
-    public InSufficientSockQuantityException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/coursework3/model/Size.java
+++ src/main/java/com/example/coursework3/model/Size.java
@@ -1,5 +1,7 @@
 package com.example.coursework3.model;
 
 public enum Size {
-    SIZE_37, SIZE_39, SIZE_41;
+    SIZE_37,
+    SIZE_39,
+    SIZE_41;
 }
