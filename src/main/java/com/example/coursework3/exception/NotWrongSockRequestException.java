bb3433c32cd0a1be82bdd882efe7a894de1bd26a
AlexandrLytkin <Dracca@mail.ru>
2023-01-19 22:21:41 +0300

    курсовая

--- src/main/java/com/example/coursework3/exception/NotWrongSockRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.coursework3.exception;
-
-public class NotWrongSockRequestException extends RuntimeException{
-    public NotWrongSockRequestException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/coursework3/model/Size.java
+++
@@ -1,6 +0,0 @@
-package com.example.coursework3.model;
-
-public enum Size {
-    SIZE_37,  SIZE_39,  SIZE_41;
-
-}
